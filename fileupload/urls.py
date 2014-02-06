2f3a8f1e50789ac1e4f0b50674e57bea83255d66
Fabrizio Buratta <fabrizio@moldiscovery.com>
2014-02-06 10:52:58 +0100

    added login and logout view

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,17 +1,21 @@
 # encoding: utf-8
 from django.conf.urls import patterns, url
+
 from fileupload.views import (
         BasicVersionCreateView, BasicPlusVersionCreateView,
         jQueryVersionCreateView, AngularVersionCreateView,
-        PictureCreateView, PictureDeleteView, PictureListView,
+        FileCreateView, FileDeleteView, FileListView,
+        login_view, logout_view,
         )
 
 urlpatterns = patterns('',
-    url(r'^basic/$', BasicVersionCreateView.as_view(), name='upload-basic'),
-    url(r'^basic/plus/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
-    url(r'^new/$', PictureCreateView.as_view(), name='upload-new'),
-    url(r'^angular/$', AngularVersionCreateView.as_view(), name='upload-angular'),
+    #url(r'^basic/$', BasicVersionCreateView.as_view(), name='upload-basic'),
+    #url(r'^basic/plus/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
+    #url(r'^new/$', FileCreateView.as_view(), name='upload-new'),
+    #url(r'^angular/$', AngularVersionCreateView.as_view(), name='upload-angular'),
+    url(r'^login/$', login_view, name='login'),
+    url(r'^logout/$', logout_view, name='logout'),
     url(r'^jquery-ui/$', jQueryVersionCreateView.as_view(), name='upload-jquery'),
-    url(r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), name='upload-delete'),
-    url(r'^view/$', PictureListView.as_view(), name='upload-view'),
+    url(r'^delete/(?P<pk>\d+)$', FileDeleteView.as_view(), name='upload-delete'),
+    url(r'^view/$', FileListView.as_view(), name='upload-view'),
 )
--- fileupload/views.py
+++ fileupload/views.py
@@ -3,13 +3,30 @@ import json
 
 from django.http import HttpResponse
 from django.views.generic import CreateView, DeleteView, ListView
-from .models import Picture
+from .models import File
 from .response import JSONResponse, response_mimetype
 from .serialize import serialize
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
+def logout_view(request):
+    logout(request)
+    return redirect('/upload/jquery-ui/')
 
-class PictureCreateView(CreateView):
-    model = Picture
+def login_view(request):
+
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+       
+    return redirect('/upload/jquery-ui/')
+
+class FileCreateView(CreateView):
+    model = File
 
     def form_valid(self, form):
         self.object = form.save()
@@ -23,24 +40,24 @@ class PictureCreateView(CreateView):
         data = json.dumps(form.errors)
         return HttpResponse(content=data, status=400, content_type='application/json')
 
-class BasicVersionCreateView(PictureCreateView):
+class BasicVersionCreateView(FileCreateView):
     template_name_suffix = '_basic_form'
 
 
-class BasicPlusVersionCreateView(PictureCreateView):
+class BasicPlusVersionCreateView(FileCreateView):
     template_name_suffix = '_basicplus_form'
 
 
-class AngularVersionCreateView(PictureCreateView):
+class AngularVersionCreateView(FileCreateView):
     template_name_suffix = '_angular_form'
 
 
-class jQueryVersionCreateView(PictureCreateView):
+class jQueryVersionCreateView(FileCreateView):
     template_name_suffix = '_jquery_form'
 
 
-class PictureDeleteView(DeleteView):
-    model = Picture
+class FileDeleteView(DeleteView):
+    model = File
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
@@ -50,8 +67,8 @@ class PictureDeleteView(DeleteView):
         return response
 
 
-class PictureListView(ListView):
-    model = Picture
+class FileListView(ListView):
+    model = File
 
     def render_to_response(self, context, **response_kwargs):
         files = [ serialize(p) for p in self.get_queryset() ]
