fed77953f4653b7bc2e72c3c4b7c46c0a20ead14
Fabrizio Buratta <fabrizio@moldiscovery.com>
2014-02-25 16:01:36 +0100

    added email notification on upload

--- fileupload/views.py
+++ fileupload/views.py
@@ -9,6 +9,33 @@ from .serialize import serialize
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+import smtplib
+
+def send_email(response,user):
+
+    FROMADDR = "uploads@ml.moldiscovery.com"
+    LOGIN    = FROMADDR
+    #PASSWORD = ""
+    TOADDRS  = ["fabrizio@moldiscovery.com"]
+    SUBJECT  = "Files uploaded"
+    msg_content = "User: %s \r\n" % user
+
+    for file in response['files']:
+        msg_content += "File Uploaded: "+ file['name'] + '\r\n' 
+        msg_content += "Download: https://ml.moldiscovery.com"+ file['url'] + '\r\n\r\n' 
+   
+    msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
+           % (FROMADDR, ", ".join(TOADDRS), SUBJECT) )
+    msg += msg_content+"\r\n"
+
+    server = smtplib.SMTP('localhost', 25)
+    #server.set_debuglevel(1)
+    server.ehlo("ml.moldiscovery.com")
+    #server.starttls()
+    #server.login(LOGIN, PASSWORD)
+    server.sendmail(FROMADDR, TOADDRS, msg)
+    server.quit()  
+
 
 def logout_view(request):
     logout(request)
@@ -25,8 +52,6 @@ def login_view(request):
        
     return redirect('home')
 
-
-
 class FileCreateView(CreateView):
     model = File
 
@@ -36,6 +61,7 @@ class FileCreateView(CreateView):
         data = {'files': files}
         response = JSONResponse(data, mimetype=response_mimetype(self.request))
         response['Content-Disposition'] = 'inline; filename=files.json'
+        send_email(data, self.request.user.username)
         return response
 
     def form_invalid(self, form):
