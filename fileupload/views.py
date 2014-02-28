bde0195d558f4021cb36013ba8f7c2f841866193
Fabrizio Buratta <fabrizio@moldiscovery.com>
2014-02-28 11:10:42 +0100

    better progress bar css

--- fileupload/views.py
+++ fileupload/views.py
@@ -22,6 +22,7 @@ def send_email(response,user):
 
     for file in response['files']:
         msg_content += "File Uploaded: "+ file['name'] + '\r\n' 
+        msg_content += "File Size: "+ str(file['size']) + ' bytes\r\n' 
         msg_content += "Download: https://ml.moldiscovery.com:8888/uploader"+ file['url'] + '\r\n\r\n' 
    
     msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
