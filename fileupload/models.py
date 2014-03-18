f8cbcfc904b18d6b79488dc86f8241afb99d4a7f
Bobo <fabrizio@moldiscovery.com>
2014-03-18 17:57:20 +0100

    added verbose name to slug

--- fileupload/models.py
+++ fileupload/models.py
@@ -12,7 +12,7 @@ class File(models.Model):
 
     """
     file = models.FileField(upload_to=upload_dir_path)
-    slug = models.SlugField(max_length=50, blank=True )
+    slug = models.SlugField(max_length=50, blank=True, verbose_name="file name")
     username = models.CharField(max_length=50)
 
     def __unicode__(self):
