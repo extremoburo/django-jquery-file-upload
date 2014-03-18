d3eaf40d11fa529a9a12b65f0a8e1b59942fda51
Bobo <fabrizio@moldiscovery.com>
2014-03-18 15:47:39 +0100

    added action to download selected files

--- fileupload/admin.py
+++ fileupload/admin.py
@@ -1,4 +1,24 @@
 from fileupload.models import File
 from django.contrib import admin
+from django.http import HttpResponse
+import tarfile
 
-admin.site.register(File)
+def make_download(modeladmin, request, queryset):
+    response = HttpResponse(mimetype='application/x-gzip')
+    response['Content-Disposition'] = 'attachment; filename=MD_secure_upload_download.tar.gz'
+    tarred = tarfile.open(fileobj=response, mode='w:gz')
+
+    for file in queryset:
+       tarred.add("media/"+file.file.name)
+    tarred.close()
+
+    return response
+    
+make_download.short_description = "Download selected files"
+
+
+class FileAdmin(admin.ModelAdmin):
+    actions = [make_download]
+   
+
+admin.site.register(File,FileAdmin)
